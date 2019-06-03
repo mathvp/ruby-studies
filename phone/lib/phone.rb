d16e5456b989a64b172085fd6615df8ecd714217
Matheus Vieira <matheusvieiradepaula@gmail.com>
2019-06-02 21:55:05 -0300

    Change phone.rb properties into private method and Adjust Rubocop Offenses

--- phone/lib/phone.rb
+++ phone/lib/phone.rb
@@ -1,19 +1,4 @@
 class Phone
-  attr_reader :nums_letters
-
-  def initialize
-    @nums_letters = { 0 => %w[0], 1 => %w[1],
-                      2 => %w[A B C],
-                      3 => %w[D E F],
-                      4 => %w[G H I],
-                      5 => %w[J K L],
-                      6 => %w[M N O],
-                      7 => %w[P Q R S],
-                      8 => %w[T U V],
-                      9 => %w[W X Y Z],
-                      '-' => %w[-] }
-  end
-
   def to_number(letter)
     nums_letters.each do |key, value|
       return key if value.include?(letter)
@@ -27,5 +12,20 @@ class Phone
       phone_number << to_number(letter).to_s
     end
     phone_number
+  end
+
+  private
+
+  def nums_letters
+    { 0 => %w[0], 1 => %w[1],
+      2 => %w[A B C],
+      3 => %w[D E F],
+      4 => %w[G H I],
+      5 => %w[J K L],
+      6 => %w[M N O],
+      7 => %w[P Q R S],
+      8 => %w[T U V],
+      9 => %w[W X Y Z],
+      '-' => %w[-] }
   end
 end
